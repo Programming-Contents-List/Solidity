dc2d9b164c45fbb20c991c61a5d27d6c375d0693
4Bee <4bee.code@gmail.com>
2025-01-15 22:34:38 +0900

    feat: edited app.tsx, applied tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,37 +8,38 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <div>
-          <h1>Thirdweb + Vite + React</h1>
-          <ConnectWallet />
-        </div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
         <h1 className="text-4xl font-bold text-white">
           Tailwind CSS 적용 완료! 🚀
-        </h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        </h1>{" "}
+        <div>
+          <div>
+            <h1>Thirdweb + Vite + React</h1>
+            <ConnectWallet />
+          </div>
+          <div className="flex justify-center">
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
